9ef2cae0dab8dca658b6d0ee768b45353c4b6be4
aymanxdev <ayman.bacc@gmail.com>
2021-06-01 23:59:30 +0100

    add mnew

--- web/src/components/Contact.js
+++ web/src/components/Contact.js
@@ -5,19 +5,26 @@ const Contact = () => {
   return (
     <div className="container-contact">
       <div className="bg-dots-contact"></div>
-      <div className="sub-container-contact">
-        <div className="title-contact">
-          <h1 className="title">
-            Get in Touch <span className="big-dot-contact">.</span>
-          </h1>
-          <p className="sub-text-contact">
-            I’m a graduate with a Master’s degree in Software Engineering and a Bachelor degree in
-            Computer Science. I enjoy creating applications that allow me to combine my background
-            in technolgoy and my interest in clean design
-          </p>
-          <button className="contact-btn">
-            <span className="btn-contact-text">Say Hello</span>{" "}
-          </button>
+      <div className="container-contact-shadow">
+        <div className="sub-container-contact">
+          <div id="contact-corner-tl" className="contact-corner"></div>
+          <div id="contact-corner-tr" className="contact-corner"></div>
+          <div id="contact-corner-bl" className="contact-corner"></div>
+          <div id="contact-corner-br" className="contact-corner"></div>
+
+          <div className="title-contact">
+            <h1 className="title">
+              Get in Touch <span className="big-dot-contact">.</span>
+            </h1>
+            <p className="sub-text-contact">
+              I’m a graduate with a Master’s degree in Software Engineering and a Bachelor degree in
+              Computer Science. I enjoy creating applications that allow me to combine my background
+              in technolgoy and my interest in clean design
+            </p>
+            <button className="contact-btn">
+              <span className="btn-contact-text">Say Hello</span>{" "}
+            </button>
+          </div>
         </div>
       </div>
     </div>
--- web/src/components/Skills.js
+++ web/src/components/Skills.js
@@ -5,31 +5,38 @@ const Skills = () => {
   return (
     <div className="container-skills">
       <div className="bg-dots-skills"></div>
-      <div className="sub-container-skills">
-        <div className="skill-title-container">
-          <h1 className="title-skill">
-            My Skills - <span className="sub-text-text"> Technologies & Tools</span>
-            <span className="big-dot-skills">.</span>
-          </h1>
-          <hr className="line-skills" />
-        </div>
+      <div className="container-skill-shadow">
+        <div className="sub-container-skills">
+          <div id="corner-tl" className="corner"></div>
+          <div id="corner-tr" className="corner"></div>
+          <div id="corner-br" className="corner"></div>
+          <div id="corner-bl" className="corner"></div>
+          <div className="skill-title-container">
+            <h1 className="title-skill">
+              My Skills - <span className="sub-text-text"> Technologies & Tools</span>
+              <span className="big-dot-skills">.</span>
+            </h1>
+            <hr className="line-skills" />
+          </div>
 
-        <div className="tech-tools-container">
-          <ul>
-            <li>Python</li>
-            <li>JavaScript (ES6)</li>
-            <li>Reactjs</li>
-            <li>REST API</li>
-            <li>MongoDB</li>
-          </ul>
-          <ul>
-            <li>MySQL</li>
-            <li>Nodejs</li>
-            <li>Firebase</li>
-            <li>Git</li>
-            <li>HTML / CSS</li>
-            <li>Teamwork</li>
-          </ul>
+          <div className="tech-tools-container">
+            <ul>
+              <li>Python</li>
+              <li>JavaScript (ES6)</li>
+              <li>Reactjs</li>
+              <li>REST API</li>
+              <li>MongoDB</li>
+            </ul>
+            <ul>
+              <li>MySQL</li>
+              <li>Nodejs</li>
+              <li>Firebase</li>
+              <li>Git</li>
+              <li>HTML / CSS</li>
+              <li>Teamwork</li>
+            </ul>
+          </div>
+          <div className="big-circle"></div>
         </div>
       </div>
     </div>
