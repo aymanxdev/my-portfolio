115de708a356fd48d29d3ef375f38f2839cc161b
aymanxdev <ayman.bacc@gmail.com>
2021-06-16 23:16:46 +0100

    nav bottom style

--- web/src/components/BottomNav.js
+++ web/src/components/BottomNav.js
@@ -1,8 +1,29 @@
 import React from "react";
 import "../components/btmNav.css";
-
+import { FaGithub } from "react-icons/fa";
+import { FaLinkedin } from "react-icons/fa";
+import { FaMedium } from "react-icons/fa";
 const BottomNav = () => {
-  return <div class="wrapper"></div>;
+  return (
+    <div class="wrapper">
+      <div className="btm-nav-icons">
+        <ul>
+          <li>
+            {" "}
+            <FaGithub />{" "}
+          </li>
+          <li>
+            {" "}
+            <FaLinkedin />{" "}
+          </li>
+          <li>
+            {" "}
+            <FaMedium />
+          </li>
+        </ul>
+      </div>
+    </div>
+  );
 };
 
 export default BottomNav;
