b54f49abbeffae8a051ea26af1070d78b63ae345
aymanxdev <ayman.bacc@gmail.com>
2023-10-24 21:49:16 +0100

    upgrade v2 to v3

--- studio/deskStructure.js
+++ studio/deskStructure.js
@@ -1,10 +1,9 @@
-import S from '@sanity/desk-tool/structure-builder'
-import { MdSettings } from "react-icons/md";
+import {MdSettings} from 'react-icons/md'
 
 const hiddenDocTypes = listItem =>
   !['category', 'person', 'sampleProject', 'siteSettings'].includes(listItem.getId())
 
-export default () =>
+export default (S) =>
   S.list()
     .title('Content')
     .items([
